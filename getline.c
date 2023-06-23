9de1982ea968cc8ea76ab6e0db68c61444d7e1ea
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-06-23 15:51:19 +0000

    A function to get inputs from a user

---
+++ getline.c
@@ -0,0 +1,47 @@
+#include "shell.h"
+
+/**
+ * _getinput - A function that gets input from STDIN
+ *
+ */
+char **_getinput(void)
+{
+	size_t size;
+	char *token = NULL, *buffer = NULL;
+	const char *delimeter = " \n";
+	int argc = 0, i;
+	char **argv = NULL;
+
+		if (getline(&buffer, &size, STDIN) == -1)
+		{
+			free(buffer);
+			return NULL;
+		}
+
+		token = strtok(buffer, delimeter);
+		while(token)
+		{
+			token = strtok(NULL, delimeter);
+			argc++;
+		}
+		argv = malloc(sizeof(char *) * (argc + 1));
+		if (argv == NULL)
+		{
+			  perror("Memory allocation failed\n");
+			  free(buffer);
+			  return NULL;
+		}
+
+		i = 0;
+		token = strtok(buffer, delimeter);
+		while(token)
+		{
+			argv[i] = strdup(token);
+			token = strtok(NULL, delimeter);
+			i++;
+		}
+		argv[i] = NULL;
+
+	free(buffer);
+	return (argv);
+}
